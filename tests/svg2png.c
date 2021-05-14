201a9cb3af17d3bdaf579cb17ac024f62125d5b3
xueshuming <xueshuming@minigui.org>
2021-05-14 14:56:24 +0800

    update license

--- tests/svg2png.c
+++ tests/svg2png.c
@@ -1,3 +1,50 @@
+/////////////////////////////////////////////////////////////////////////////// //
+//                          IMPORTANT NOTICE
+//
+// The following open source license statement does not apply to any
+// entity in the Exception List published by FMSoft.
+//
+// For more information, please visit:
+//
+// https://www.fmsoft.cn/exception-list
+//
+//////////////////////////////////////////////////////////////////////////////
+/**
+ \verbatim
+
+    This file is part of hiSVG. hiSVG is a  high performance SVG
+    rendering library.
+
+    Copyright (C) 2021 Beijing FMSoft Technologies Co., Ltd.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General License for more details.
+
+    You should have received a copy of the GNU Lesser General License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    Or,
+
+    As this program is a library, any link to this program must follow
+    GNU Lesser General License version 3 (LGPLv3). If you cannot accept
+    LGPLv3, you need to be licensed from FMSoft.
+
+    If you have got a commercial license of this program, please use it
+    under the terms and conditions of the commercial license.
+
+    For more information about the commercial license, please refer to
+    <http://www.minigui.com/blog/minigui-licensing-policy/>.
+
+ \endverbatim
+ */
+
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -57,9 +104,9 @@ int MiniGUIMain (int argc, const char* argv[])
     cairo_paint (cr);
 #endif
 
-    HiSVGRect rect = {0, 0, 100, 100};
-    //hisvg_handle_render_cairo (svg, cr, &rect,  NULL, NULL);
-    hisvg_handle_render_cairo (svg, cr, NULL,  NULL, NULL);
+    HiSVGRect rect = {0, 0, 200, 200};
+    hisvg_handle_render_cairo (svg, cr, &rect,  NULL, NULL);
+//    hisvg_handle_render_cairo (svg, cr, NULL,  NULL, NULL);
     cairo_surface_write_to_png (surface, png_name);
 
     cairo_surface_destroy (surface);
